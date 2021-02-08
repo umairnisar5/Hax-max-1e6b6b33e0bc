de8d839658cd51362af16e0c7820fe50fb2cc10d
umairnisar5 <49435408+umairnisar5@users.noreply.github.com>
2021-02-08 20:20:04 +0500

    feth api data in meta mask

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 import HexMax from "./Views/hexMax";
+import Connect from "./Views/Connect";
 
 function App() {
   return (
     <div className="App">
       <HexMax />
+      <Connect />
     </div>
   );
 }
